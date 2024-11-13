24f93e50adbaf09b18bb50f1f62a07132e3daa45
Taylor Smith <taylorstevensmith07@gmail.com>
2024-11-13 00:10:32 +0000

    added message to say that the specific vehicle type has been added to the database and made it so the database can have no more than 3 cars and 3 vans. If more data tries to get added to either list, an error message comes saying the database is full of that type of vehicle.

--- VehicleDatabse/Form1.cs
+++ VehicleDatabse/Form1.cs
@@ -4,9 +4,8 @@ namespace VehicleDatabse
 {
     public partial class Form1 : Form
     {
-        Van van = new Van();
-
         List<Car> carsList = new List<Car>();
+        List<Van> vansList = new List<Van>();
 
         public Form1()
         {
@@ -17,6 +16,7 @@ namespace VehicleDatabse
         private void showInfo_btn_Click(object sender, EventArgs e)
         {
             Car car = new Car();
+            Van van = new Van();
 
             switch (carVan.Text == "Car")
             {
@@ -32,7 +32,6 @@ namespace VehicleDatabse
                             else
                             {
                                 car = carsList.ElementAt(0);
-                                car.vehicleNumber = vehicleSelect.Items.IndexOf("Car 1");
                                 infoArea.Text = "Registration Number: " + car.registration + "\r\nWheels: " + car.wheels + "\r\nWeight (KG): " + car.weight + "\r\nPassengers: " + car.passengers;
                                 break;
                             }
@@ -46,7 +45,6 @@ namespace VehicleDatabse
                             else
                             {
                                 car = carsList.ElementAt(1);
-                                car.vehicleNumber = vehicleSelect.Items.IndexOf("Car 2");
                                 infoArea.Text = "Registration Number: " + car.registration + "\r\nWheels: " + car.wheels + "\r\nWeight (KG): " + car.weight + "\r\nPassengers: " + car.passengers;
                                 break;
                             }
@@ -54,12 +52,12 @@ namespace VehicleDatabse
                         case "Car 3":
                             if (carsList.Count != 3)
                             {
+                                infoArea.Text = "Car doesn't contain any data";
                                 break;
                             }
                             else
                             {
                                 car = carsList.ElementAt(2);
-                                car.vehicleNumber = vehicleSelect.Items.IndexOf("Car 3");
                                 infoArea.Text = "Registration Number: " + car.registration + "\r\nWheels: " + car.wheels + "\r\nWeight (KG): " + car.weight + "\r\nPassengers: " + car.passengers;
                                 break;
                             }
@@ -73,27 +71,56 @@ namespace VehicleDatabse
                     switch (vehicleSelect.SelectedItem)
                     {
                         case "Van 1":
-                            infoArea.Text = "Registration Number: " + van.registration + "\r\nWheels: " + van.wheels + "\r\nWeight (KG): " + van.weight + "\r\nHeight: " + van.height;
-                            break;
+                            if (vansList.Count == 0)
+                            {
+                                infoArea.Text = "Van doesn't contain any data";
+                                break;
+                            }
+                            else
+                            {
+                                van = vansList.ElementAt(0);
+                                van.vehicleNumber = vehicleSelect.Items.IndexOf("Van 1");
+                                infoArea.Text = "Registration Number: " + van.registration + "\r\nWheels: " + van.wheels + "\r\nWeight (KG): " + van.weight + "\r\nHeight: " + van.height;
+                                break;
+                            }
+
                         case "Van 2":
-                            infoArea.Text = "Registration Number: " + van.registration + "\r\nWheels: " + van.wheels + "\r\nWeight (KG): " + van.weight + "\r\nHeight: " + van.height;
-                            break;
+                            if (vansList.Count != 2)
+                            {
+                                infoArea.Text = "Van doesn't contain any data";
+                                break;
+                            }
+                            else
+                            {
+                                van = vansList.ElementAt(0);
+                                van.vehicleNumber = vehicleSelect.Items.IndexOf("Van 2");
+                                infoArea.Text = "Registration Number: " + van.registration + "\r\nWheels: " + van.wheels + "\r\nWeight (KG): " + van.weight + "\r\nHeight: " + van.height;
+                                break;
+                            }
                         case "Van 3":
-                            infoArea.Text = "Registration Number: " + van.registration + "\r\nWheels: " + van.wheels + "\r\nWeight (KG): " + van.weight + "\r\nHeight: " + van.height;
-                            break;
+                            if (vansList.Count != 3)
+                            {
+                                infoArea.Text = "Van doesn't contain any data";
+                                break;
+                            }
+                            else
+                            {
+                                van = vansList.ElementAt(0);
+                                van.vehicleNumber = vehicleSelect.Items.IndexOf("Van 3");
+                                infoArea.Text = "Registration Number: " + van.registration + "\r\nWheels: " + van.wheels + "\r\nWeight (KG): " + van.weight + "\r\nHeight: " + van.height;
+                                break;
+                            }
                         default:
                             infoArea.Text = "Error showing information";
                             break;
                     }
                     break;
             }
-
-            Console.WriteLine(car.vehicleNumber);
-            Console.WriteLine(carsList.Count());
         }
 
         private void selectBtn_Click(object sender, EventArgs e)
         {
+
             if (carVan.Text == "Car")
             {
                 vehicleSelect.Items.Clear();
@@ -117,12 +144,23 @@ namespace VehicleDatabse
         }
         private void addVehicleBtn_Click(object sender, EventArgs e)
         {
-            AddVehicle();
+            if (carVanAdd.SelectedItem == "Car" && carsList.Count >= 3 || carVanAdd.SelectedItem == "Van" && vansList.Count >= 3)
+            {
+                infoArea.Text = "The database is full of this vehicle type and therefore couldn't be added.";
+                Console.WriteLine("database of vehicles is full, couldnt add vehicle");
+            }
+            else
+            {
+                AddVehicle();
+                Console.WriteLine("added a vehicle to the database");
+            }
+
         }
 
         private void AddVehicle()
         {
             Car car = new Car();
+            Van van = new Van();
 
             if (carVanAdd.SelectedItem == "Car")
             {
@@ -130,8 +168,8 @@ namespace VehicleDatabse
                 car.wheels = wheelsText.Text;
                 car.weight = double.Parse(weightText.Text);
                 car.passengers = int.Parse(passengersText.Text);
-                car.vehicleNumber++;
                 carsList.Add(car);
+                infoArea.Text = "Vehicle Information added to the Cars list";
             }
             else if (carVanAdd.SelectedItem == "Van")
             {
@@ -140,7 +178,10 @@ namespace VehicleDatabse
                 van.weight = double.Parse(weightText.Text);
                 van.height = int.Parse(heightText.Text);
                 van.vehicleNumber++;
-                // add van to van list
+                vansList.Add(van);
+                infoArea.Text = "Vehicle Information added to the Vans list ";
+
+                // add error check for missing datas
             }
             else
             {
