8157865d8e5991770d05163c02483c27bc7b6469
taylor <taylorstevensmith07@gmail.com>
2024-11-25 15:15:00 +0000

    even more code cleaning.

--- VehicleDatabse/Form1.cs
+++ VehicleDatabse/Form1.cs
@@ -79,7 +79,7 @@ namespace VehicleDatabse
                             else
                             {
                                 van = vansList.ElementAt(0);
-                                van.vehicleNumber = vehicleSelect.Items.IndexOf("Van 1");
+                                //van.vehicleNumber = vehicleSelect.Items.IndexOf("Van 1");
                                 infoArea.Text = "Registration Number: " + van.registration + "\r\nWheels: " + van.wheels + "\r\nWeight (KG): " + van.weight + "\r\nHeight: " + van.height;
                                 break;
                             }
@@ -93,7 +93,7 @@ namespace VehicleDatabse
                             else
                             {
                                 van = vansList.ElementAt(1);
-                                van.vehicleNumber = vehicleSelect.Items.IndexOf("Van 2");
+                                //van.vehicleNumber = vehicleSelect.Items.IndexOf("Van 2");
                                 infoArea.Text = "Registration Number: " + van.registration + "\r\nWheels: " + van.wheels + "\r\nWeight (KG): " + van.weight + "\r\nHeight: " + van.height;
                                 break;
                             }
@@ -106,7 +106,7 @@ namespace VehicleDatabse
                             else
                             {
                                 van = vansList.ElementAt(1);
-                                van.vehicleNumber = vehicleSelect.Items.IndexOf("Van 3");
+                                //van.vehicleNumber = vehicleSelect.Items.IndexOf("Van 3");
                                 infoArea.Text = "Registration Number: " + van.registration + "\r\nWheels: " + van.wheels + "\r\nWeight (KG): " + van.weight + "\r\nHeight: " + van.height;
                                 break;
                             }
@@ -193,7 +193,7 @@ namespace VehicleDatabse
                     van.wheels = wheelsText.Text;
                     van.weight = double.Parse(weightText.Text);
                     van.height = double.Parse(heightText.Text);
-                    van.vehicleNumber++;
+                   // van.vehicleNumber++;
                     infoArea.Text = "Vehicle Information added to the Vans list ";
                 }
             }
